d349b4b75ca5037fa3e00206542e27e48af38957
oxfordzkikoks <olaf.chojnacki04@wp.pl>
2021-07-05 23:05:22 +0200

    Accelerometer values reading via UART.

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ import serial.rs485
 
 #python -m serial.tools.list_ports      ////////  show all ports in console
 
-ser = serial.Serial('COM6', 38400, timeout=0.75)
+ser = serial.Serial('COM5', 115200, timeout=1)
 print(ser.name)
 print(ser.timeout)
-
-modbus = serial.rs485.RS485()
-modbus.rs485_mode = serial.rs485.RS485Settings()
-
+print(ser.is_open)
+x=0
+while x < 200:
+    print(ser.read(50))
+    x=x+1
 
 ser.close()
 
